2f5ab135a6294132cd6225743524076adcd799db
Eric J. Humphrey <ejhumphrey@nyu.edu>
2016-01-21 22:04:45 -0500

    Updated core tests to pytest.

--- biggie/tests/test_core.py
+++ biggie/tests/test_core.py
@@ -1,59 +1,50 @@
-import unittest
+import pytest
 import numpy as np
 
 import biggie
 
 
-class TestCore(unittest.TestCase):
+@pytest.fixture(scope='module')
+def data():
+    some_int = 3
+    some_str = 'im_a_string'
+    some_list = [1, 2, 3]
+    some_ndarray = np.arange(5)
 
-    def setUp(self):
-        self.some_int = 3
-        self.some_str = 'im_a_string'
-        self.some_list = [1, 2, 3]
-        self.some_ndarray = np.arange(5)
-        self.entity = biggie.Entity(
-            a=self.some_int, b=self.some_str,
-            c=self.some_list, d=self.some_ndarray)
+    class Sample(object):
+        a = some_int
+        b = some_str
+        c = some_list
+        d = some_ndarray
+        e = biggie.Entity(
+            a=some_int, b=some_str,
+            c=some_list, d=some_ndarray)
 
-    def tearDown(self):
-        pass
+    return Sample
 
-    def test_getitem(self):
-        self.assertEqual(
-            self.entity.a,
-            self.entity['a'].value,
-            "Failed to initialize keys / attributes.")
 
-    def test_int_field(self):
-        self.assertEqual(
-            self.entity.a,
-            self.some_int,
-            "Failed to initialize an int.")
+def test_Entity_getitem(data):
+    assert data.e.a == data.e['a'].value, \
+        "Failed to initialize keys / attributes."
 
-    def test_str_field(self):
-        self.assertEqual(
-            self.entity.b.tostring(),
-            self.some_str,
-            "Failed to initialize a string.")
 
-    def test_list_field(self):
-        self.assertEqual(
-            self.entity.c.tolist(),
-            self.some_list,
-            "Failed to initialize a list.")
+def test_Entity_int_field(data):
+    assert data.e.a == data.a, "Failed to initialize an int."
 
-    def test_ndarray_field(self):
-        np.testing.assert_array_equal(
-            self.entity.d,
-            self.some_ndarray,
-            "Failed to initialize a numpy array.")
 
-    def test_values(self):
-        values = self.entity.values()
-        self.assertEqual(
-            values['a'],
-            self.some_int,
-            "Failed to initialize a numpy array.")
+def test_Entity_str_field(data):
+    assert data.e.b.tostring() == data.b, "Failed to initialize a string."
 
-if __name__ == "__main__":
-    unittest.main()
+
+def test_Entity_list_field(data):
+    assert data.e.c.tolist() == data.c, "Failed to initialize a list."
+
+
+def test_Entity_ndarray_field(data):
+    np.testing.assert_array_equal(
+        data.e.d, data.d, "Failed to initialize a numpy array.")
+
+
+def test_Entity_values(data):
+    values = data.e.values()
+    assert values['a'] == data.a, "Failed to initialize a numpy array."
